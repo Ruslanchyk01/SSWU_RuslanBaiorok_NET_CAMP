2dbdaaf95dee8d1f79aa9de6437fad251c480d41
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-24 07:07:53 +0300

    Update Program.cs

--- Home_task_4/Task3/Program.cs
+++ Home_task_4/Task3/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Globalization;
 using Task3;
-
+//там трішки в іншому форматі дата мала бути) Але загалом - все добре.
 CultureInfo.CurrentCulture = new CultureInfo("uk-UA");
 
 Menu.ShowMenu();
